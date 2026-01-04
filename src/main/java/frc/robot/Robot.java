332f730da9caec583aa91cca4db75820139fa93e
Ian Galaviz <brianatthehouse@gmail.com>
2026-01-04 15:34:26 -0700

    3 piece auto achieved in sim

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -107,9 +107,7 @@ public class Robot extends LoggedRobot {
 
     /** This function is called once when the robot is disabled. */
     @Override
-    public void disabledInit() {
-        robotContainer.resetSimulationField();
-    }
+    public void disabledInit() {}
 
     /** This function is called periodically when disabled. */
     @Override
@@ -162,7 +160,9 @@ public class Robot extends LoggedRobot {
 
     /** This function is called once when the robot is first started up. */
     @Override
-    public void simulationInit() {}
+    public void simulationInit() {
+        robotContainer.resetSimulationField();
+    }
 
     /** This function is called periodically whilst in simulation. */
     @Override
